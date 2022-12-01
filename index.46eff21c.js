d942b7f31b73da48fae5677c25694deb9dcbf234
GHKirill <103307710+GHKirill@users.noreply.github.com>
2022-12-01 06:43:51 +0000

    Deploying to gh-pages from @ GHKirill/repo-parcel-01@8a022fd7fffb97f172ae608363a5b4163fcbe9bd 🚀

--- index.46eff21c.js
+++
@@ -1,2 +0,0 @@
-$(".multiple-items").slick({arrows:!0,dots:!1,infinite:!0,slidesToShow:2,slidesToScroll:1}),$(".variable-width").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,centerMode:!0,variableWidth:!0});
-//# sourceMappingURL=index.46eff21c.js.map
